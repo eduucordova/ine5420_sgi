29fe6862f8412f72a53d57cebcb83b61f7d8cd04
Eduardo Cordova <eduucordova@gmail.com>
2017-03-18 18:04:18 -0300

    viewport

--- src/Viewport.hpp
+++ src/Viewport.hpp
@@ -3,20 +3,84 @@
 #ifndef VIEWPORT_HPP_
 #define VIEWPORT_HPP_
 
+#include <stdlib.h>
+#include <iostream>
+#include <list>
+
+int xMax, yMax;
+cairo_t *cr;
+
 class ViewPort {
 public:
     ViewPort(int width, int height)
-    : xMax(height)
-    , yMax(width)
     {
-
+        xMax = height;
+        yMax = width;
     }
 
     ~ViewPort() {}
 
+    void draw(int type, std::list<Coordinate*> *coordinates) {
+        cr = cairo_create(surface);
 
+        switch (type) {
+            case 0:
+            {
+                Coordinate *coordinate = coordinates->front();
+                cairo_set_line_width(cr, 1);
+                cairo_arc(cr, coordinate->getX(), coordinate->getY(), 1, 0, 10);
+                cairo_stroke_preserve(cr);
+                cairo_fill(cr);
+                break;
+            }
+            case 1:
+                std::list<Coordinate*>::iterator nextCoord;
+                for (std::list<Coordinate*>::iterator it = coordinates->begin(); it != coordinates->end(); it++) {
+                    if (std::next(it, 1) == coordinates->end())
+                        nextCoord = coordinates->begin();
+                    else
+                        nextCoord = std::next(it, 1);
+
+                    cairo_move_to(cr, (*it)->getX(), (*it)->getY());
+                    cairo_line_to(cr, (*nextCoord)->getX(), (*nextCoord)->getY());
+                }
+                cairo_stroke(cr);
+                gtk_widget_queue_draw_area (drawing_area, 0 , 0, yMax, xMax);
+                break;
+        }
+    }
 private:
-    int xMax, yMax;
 };
+
+/*Clear the surface, removing the scribbles*/
+static void clear_surface (){
+    cr = cairo_create (surface);
+
+    cairo_set_source_rgb (cr, 1, 1, 1);
+    cairo_paint (cr);
+
+    cairo_destroy (cr);
+}
+
+/*Creates the surface*/
+static gboolean create_surface (GtkWidget *widget, GdkEventConfigure *event, gpointer data){
+    if (surface)
+        cairo_surface_destroy (surface);
+
+    surface = gdk_window_create_similar_surface (gtk_widget_get_window (widget),
+                                                CAIRO_CONTENT_COLOR,
+                                                gtk_widget_get_allocated_width (widget),
+                                                gtk_widget_get_allocated_height (widget));
+    clear_surface ();
+    return TRUE;
+}
+
+/* Redraw the screen from the surface */
+static gboolean redraw (GtkWidget *widget, cairo_t *cr, gpointer data){
+    cairo_set_source_surface (cr, surface, 0, 0);
+    cairo_paint (cr);
+
+    return FALSE;
+}
 
 #endif /* end of include guard: VIEWPORT_HPP_ */
--- src/Windows.hpp
+++ src/Windows.hpp
@@ -9,8 +9,9 @@
 #include "Viewport.hpp"
 
 #include <gtk/gtk.h>
+#include <stdlib.h>
 #include <iostream>
-#include <memory>
+#include <list>
 
 class ViewPort;
 class Window;
@@ -23,6 +24,8 @@ struct {
   double coordx[100];
   double coordy[100];
 } glob;
+
+std::list<Coordinate*> coordinates;
 
 namespace UI {
     class MainWindow {
@@ -55,38 +58,7 @@ namespace UI {
             return 0;
         }
 
-        /*Clear the surface, removing the scribbles*/
-        static void clear_surface (){
-          cairo_t *cr;
 
-          cr = cairo_create (surface);
-
-          cairo_set_source_rgb (cr, 1, 1, 1);
-          cairo_paint (cr);
-
-          cairo_destroy (cr);
-        }
-
-        /*Creates the surface*/
-        static gboolean create_surface (GtkWidget *widget, GdkEventConfigure *event, gpointer data){
-          if (surface)
-            cairo_surface_destroy (surface);
-
-          surface = gdk_window_create_similar_surface (gtk_widget_get_window (widget),
-                                               CAIRO_CONTENT_COLOR,
-                                               gtk_widget_get_allocated_width (widget),
-                                               gtk_widget_get_allocated_height (widget));
-          clear_surface ();
-          return TRUE;
-        }
-
-        /* Redraw the screen from the surface */
-        static gboolean redraw (GtkWidget *widget, cairo_t   *cr,  gpointer   data){
-          cairo_set_source_surface (cr, surface, 0, 0);
-          cairo_paint (cr);
-
-          return FALSE;
-        }
     };
 
     class AddFigureWindow {
@@ -116,10 +88,6 @@ namespace UI {
         gtk_statusbar_push(status_bar, context_id, msg);
     }
 }
-
-#include <stdlib.h>
-#include <iostream>
-#include <vector>
 
 // MainWindow events
 extern "C"
@@ -167,9 +135,6 @@ G_MODULE_EXPORT {
     }
 }
 
-
-
-
 // AddFigureWindow EVENTS_HPP_
 extern "C"
 G_MODULE_EXPORT {
@@ -179,24 +144,16 @@ G_MODULE_EXPORT {
     }
 
     void on_btn_add_point_clicked() {
+        if (!coordinates.empty())
+            coordinates.clear();
+
         double x = atof(gtk_entry_get_text(entry_ponto_x));
         double y = atof(gtk_entry_get_text(entry_ponto_y));
 
-        // Coordinate *coord = new Coordinate(x, y);
-        //
-        // window->AddPoint(coord);
-        cairo_t *cr;
+        coordinates.push_back(new Coordinate(x, y));
+        viewPort->draw(0, &coordinates);
 
-      	cr = cairo_create(surface);
-
-      	cairo_set_line_width(cr, 1);
-      	cairo_arc(cr, x, y, 1, 0, 10);
-      	cairo_stroke_preserve(cr);
-      	cairo_fill(cr);
-
-    	 cairo_destroy(cr);
-
-        gtk_widget_queue_draw_area(drawing_area, 0 , 0, 900, 900);
+        coordinates.clear();
 
         gtk_widget_hide(window_add_figure);
 
@@ -206,21 +163,24 @@ G_MODULE_EXPORT {
     }
 
     void on_btn_add_line_clicked() {
+        if (!coordinates.empty())
+            coordinates.clear();
+
         double x1 = atof(gtk_entry_get_text(entry_ponto_x1));
         double y1 = atof(gtk_entry_get_text(entry_ponto_y1));
         double x2 = atof(gtk_entry_get_text(entry_ponto_x2));
         double y2 = atof(gtk_entry_get_text(entry_ponto_y2));
 
-        cairo_t *cr;
-        cr = cairo_create (surface);
-        cairo_move_to(cr, x1, y1);
-        cairo_line_to(cr, x2, y2);
-        cairo_stroke(cr);
-        gtk_widget_queue_draw_area (drawing_area, 0 , 0, 900, 900);
+        coordinates.push_back(new Coordinate(x1, y1));
+        coordinates.push_back(new Coordinate(x2, y2));
+
+        viewPort->draw(1, &coordinates);
+
+        coordinates.clear();
 
         gtk_widget_hide(window_add_figure);
 
-        char str[50];
+        char str[255];
         sprintf(str, "line from: %f; %f to %f; %f", x1, y1, x2, y2);
         UI::write_status(str);
     }
@@ -228,33 +188,21 @@ G_MODULE_EXPORT {
     void on_btn_add_coordenada_clicked() {
         double x3 = atof(gtk_entry_get_text(entry_ponto_x3));
         double y3 = atof(gtk_entry_get_text(entry_ponto_y3));
-        glob.coordx[glob.count] = x3;
-        glob.coordy[glob.count++] = y3;
 
-        char str[50];
+        coordinates.push_back(new Coordinate(x3, y3));
+
+        char str[255];
         sprintf(str, "added coordinate at: %f; %f", x3, y3);
         UI::write_status(str);
     }
 
     void on_btn_add_polygon_clicked(){
-        cairo_t *cr;
-        cr = cairo_create (surface);
-
-        int i, j;
-        for(i = 0; i <= glob.count - 1; i++) {
-            if(i < glob.count - 1)
-                j = i + 1;
-            else
-                j = 0;
-            cairo_move_to(cr, glob.coordx[i], glob.coordy[i]);
-            cairo_line_to(cr, glob.coordx[j], glob.coordy[j]);
-        }
-
-        glob.count = 0;
-        cairo_stroke(cr);
-        gtk_widget_queue_draw_area (drawing_area, 0 , 0, 900, 900);
+        viewPort->draw(1, &coordinates);
+        
+        coordinates.clear();
 
         gtk_widget_hide(window_add_figure);
+        UI::write_status("drawing polygon");
     }
 }
 
